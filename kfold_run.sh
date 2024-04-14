bb903574a82cc517625920622b4d3084daa03065
snoop2head <snoop2head@kaist.ac.kr>
2024-04-14 22:13:16 +0900

    change config && rerun

--- kfold_run.sh
+++ kfold_run.sh
@@ -1,9 +1,5 @@
-cd /home/yjahn/joint-learning
 source /home/yjahn/miniconda3/bin/activate
 conda activate ehr
-python train.py ./config/flan-t5-large.yaml data.kfold_split=0
-python train.py ./config/flan-t5-large.yaml data.kfold_split=1
-python train.py ./config/flan-t5-large.yaml data.kfold_split=2
 python train.py ./config/flan-t5-large.yaml data.kfold_split=3
 python train.py ./config/flan-t5-large.yaml data.kfold_split=4
 python train.py ./config/flan-t5-large.yaml data.kfold_split=5
